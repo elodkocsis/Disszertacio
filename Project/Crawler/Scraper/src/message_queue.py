c0b259892bbe2c24a670876101ebf24670c22531
Előd Kocsis <elod.kocsis@outlook.com>
2022-06-02 21:21:50 +0300

    MQ fault tolerance added for the Scraper; Added local config file; Small refactoring

--- Project/Crawler/Scraper/src/message_queue.py
+++ Project/Crawler/Scraper/src/message_queue.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import time
 from typing import Dict, Optional, List, Callable
 
 from pika import BlockingConnection, ConnectionParameters, BasicProperties
@@ -8,7 +9,6 @@ from pika.spec import PERSISTENT_DELIVERY_MODE
 from src.utils import eprint, is_onion_link, dict_has_necessary_keys
 
 
-# TODO: I written this class without testing it. Check out if the message sending/acknowledging logic is correct.
 class MessageQueue:
     # keys to look for on the connection parameter dictionary
     __connection_keys = ["mq_host", "mq_port", "mq_worker_queue", "mq_processor_queue"]
@@ -22,29 +22,16 @@ class MessageQueue:
 
         """
 
-        # connect to the message queue
-        # this thing could be done with propagating exceptions, but I don't want to write a try-except in main...
-        # TODO: add some retry feature or something if the MQ goes down
-        if (connection := MessageQueue._get_connection_from_params(param_dict=param_dict,
-                                                                   needed_keys=MessageQueue.__connection_keys)) is None:
-            # at this point the errors have been printed to the screen
-            sys.exit(1)
-        else:
-            self.connection = connection
-
-        try:
-            self.channel = self.connection.channel()
-        except Exception as e:
-            eprint(f"Couldn't create channel object: {e}")
-            sys.exit(2)
-
-        # declare the scheduler/processor queue
-        # Change index for connection keys list if the ordering changes!
-        # making it durable for persistence purposes
-        self.channel.queue_declare(queue=MessageQueue.__connection_keys[3], durable=True)
+        # saving param dict
+        self.param_dict = param_dict
 
         # save the function for later
         self.function_to_execute = function_to_execute
+
+        # trying to establish connection to the MQ
+        if not self._connect(param_dict=self.param_dict):
+            eprint(f"Couldn't connect to the MQ!")
+            sys.exit(1)
 
     def close_connection(self):
         """
@@ -60,21 +47,93 @@ class MessageQueue:
         Method which reads data from the MQ, executes the task received and sends back the results.
 
         """
+        was_consuming_before = False
 
-        # declare the worker queue
-        # making it durable for persistence purposes
-        self.channel.queue_declare(queue=MessageQueue.__connection_keys[2], durable=True)
+        while True:
+            if not self.channel.is_open:
+                self._connect(param_dict=self.param_dict)
 
-        # setting basic_qos for fair dispatching
-        self.channel.basic_qos(prefetch_count=1)
+            try:
+                # declare the worker queue
+                # making it durable for persistence purposes
+                self.channel.queue_declare(queue=MessageQueue.__connection_keys[2], durable=True)
+            except Exception as e:
+                eprint(f"Exception when trying to declare queue {MessageQueue.__connection_keys[2]}: {e}")
+                # if this is the first time reaching this point, and the queue declaration failed, exit
+                if not was_consuming_before:
+                    return
+                eprint("Retrying in 10 seconds...")
+                time.sleep(10)
+                continue
 
-        # define where and how to consume
-        # at this point, the connection keys have been checked
-        # Change index for connection keys list if the ordering changes!
-        self.channel.basic_consume(queue=MessageQueue.__connection_keys[2], on_message_callback=self._on_message())
+            # setting basic_qos for fair dispatching
+            try:
+                self.channel.basic_qos(prefetch_count=1)
+            except Exception as e:
+                eprint(f"Exception when defining basic_qos: {e}")
+                # if this is the first time reaching this point, exit
+                if not was_consuming_before:
+                    return
+                eprint("Retrying in 10 seconds...")
+                time.sleep(10)
+                continue
 
-        # start consuming when data is available on the queue
-        self.channel.start_consuming()
+            try:
+                # define where and how to consume
+                # at this point, the connection keys have been checked
+                # Change index for connection keys list if the ordering changes!
+                self.channel.basic_consume(queue=MessageQueue.__connection_keys[2],
+                                           on_message_callback=self._on_message())
+            except Exception as e:
+                eprint(f"Exception when defining consume method: {e}")
+                # if this is the first time reaching this point, exit
+                if not was_consuming_before:
+                    return
+                eprint("Retrying in 10 seconds...")
+                time.sleep(10)
+                continue
+
+            try:
+                # start consuming when data is available on the queue
+                self.channel.start_consuming()
+            except Exception as e:
+                # if the MQ goes down, we will try to reconnect to the MQ
+                eprint(f"Exception when trying to start consuming messages: {e}")
+                eprint("Retrying in 10 seconds...")
+                time.sleep(10)
+                was_consuming_before = True
+
+    def _connect(self, param_dict: Dict) -> bool:
+        """
+        Function which establishes a connection to the MQ.
+
+        :param param_dict: :param param_dict: Dictionary containing the connection parameters for the MQ.
+        :return: True if a connection could be established to the MQ, otherwise False.
+
+        """
+
+        # connect to the message queue
+        if (connection := MessageQueue._get_connection_from_params(param_dict=param_dict,
+                                                                   needed_keys=MessageQueue.__connection_keys)) is None:
+            # at this point the errors have been printed to the screen
+            return False
+        else:
+            self.connection = connection
+
+        try:
+            self.channel = self.connection.channel()
+        except Exception:
+            return False
+
+        try:
+            # declare the scheduler/processor queue
+            # Change index for connection keys list if the ordering changes!
+            # making it durable for persistence purposes
+            self.channel.queue_declare(queue=MessageQueue.__connection_keys[3], durable=True)
+        except Exception:
+            return False
+
+        return True
 
     def _on_message(self):
         """
@@ -102,7 +161,7 @@ class MessageQueue:
             # run the job with the url
             result = self.function_to_execute(url)
 
-            print(" [x] URL {url} processed.")
+            print(f" [x] URL {url} processed.")
 
             # send back the result
             if self._send_message(data=result):
@@ -135,7 +194,7 @@ class MessageQueue:
                 routing_key=MessageQueue.__connection_keys[3],  # send the message to the scheduler/processor
                 body=message,
                 properties=BasicProperties(
-                    delivery_mode=PERSISTENT_DELIVERY_MODE
+                    delivery_mode=PERSISTENT_DELIVERY_MODE  # persisting message
                 ))
         except Exception as e:
             eprint(f"Couldn't send message: {e}")
