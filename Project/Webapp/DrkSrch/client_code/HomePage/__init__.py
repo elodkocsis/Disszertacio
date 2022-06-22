43211362343d42ad002de17f9bbaeb6d6e004f13
Előd Kocsis <elod.kocsis@outlook.com>
2022-06-23 01:08:16 +0300

    Webapp code refactored

--- Project/Webapp/DrkSrch/client_code/HomePage/__init__.py
+++ Project/Webapp/DrkSrch/client_code/HomePage/__init__.py
@@ -1,55 +1,30 @@
 from ._anvil_designer import HomePageTemplate
 from anvil import *
 import anvil.server
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 
 from ..ResultPage import ResultPage
+from ..Utils import get_heartbeat
+
 
 class HomePage(HomePageTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
 
-    
-  # -------------------------------
-  # Callbacks
-  # -------------------------------
-  
-  def search_card_search_event(self, query, **event_args):
-    """This method is called Event fired when the user clicks the search button or hits enter in the search box."""
-    
-    # check if the length of the query is at least 1 char
-    if len(query) > 0:
-      
-      # check if server is online
-      if self.get_heartbeat():
-        # init result page
-        result_page_form = ResultPage(query=query)
-        
-        # navigate to form
-        open_form(result_page_form)
-   
-  
-  # -------------------------------
-  # Methods
-  # -------------------------------
-  
-  def get_heartbeat(self) -> bool:
-    """Method which checks if the server is online."""
-    try:
-      return anvil.server.call('heartbeat')
-    except Exception as e:
-      print(f"Exception while trying to call heartbeat: {e}")
-      self.show_error_notification(message="Couldn't connect to server!")
-    
-    return False
-  
-  def show_error_notification(self, message: str):
-    """Method which shows an error notification."""
-    Notification(message,
-             title="Server error",
-             style="danger").show()
+    # -------------------------------
+    # Callbacks
+    # -------------------------------
 
+    def search_card_search_event(self, query, **event_args):
+        """This method is called Event fired when the user clicks the search button or hits enter in the search box."""
 
+        # check if the length of the query is at least 1 char
+        if len(query) > 0:
+
+            # check if server is online
+            if get_heartbeat():
+                # init result page
+                result_page_form = ResultPage(query=query)
+
+                # navigate to form
+                open_form(result_page_form)
--- Project/Webapp/DrkSrch/client_code/ResultPage/__init__.py
+++ Project/Webapp/DrkSrch/client_code/ResultPage/__init__.py
@@ -1,74 +1,64 @@
 from ._anvil_designer import ResultPageTemplate
 from anvil import *
 import anvil.server
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
+
+from ..Utils import get_heartbeat, show_notification
 
 
 class ResultPage(ResultPageTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
 
-    # get the query passed on from the HomePage
-    query = properties['query']
-    
-    # check if it has anything in it
-    if len(query) > 0:
-      # load the data
-      self.get_search_results(query=query)
+        # get the query passed on from the HomePage
+        query = properties['query']
 
-  # -------------------------------
-  # Callbacks
-  # -------------------------------
-  
-  def app_logo_mouse_up(self, x, y, button, **event_args):
-    """This method is called when a mouse button is released on this component"""
-    
-    open_form('HomePage')
+        # check if it has anything in it
+        if len(query) > 0:
+            # load the data
+            self.get_search_results(query=query)
 
-  def search_card_search_event(self, query, **event_args):
-    """This method is called Event fired when the user clicks the search button or hits enter in the search box."""
+    # -------------------------------
+    # Callbacks
+    # -------------------------------
+
+    def app_logo_mouse_up(self, x, y, button, **event_args):
+        """This method is called when a mouse button is released on this component"""
+
+        open_form('HomePage')
+
+    def search_card_search_event(self, query, **event_args):
+        """This method is called Event fired when the user clicks the search button or hits enter in the search box."""
+
+        # check if the length of the query is at least 1 char
+        if len(query) > 0:
+            self.get_search_results(query=query)
+
+    # -------------------------------
+    # Methods
+    # -------------------------------
+
+    def get_search_results(self, query: str, num: int = 100):
+        """
+        Method which attempts to retrieve the search results for a query.
     
-    # check if the length of the query is at least 1 char
-    if len(query) > 0:
-      self.get_search_results(query=query)
-        
-        
-  # -------------------------------
-  # Methods
-  # -------------------------------
-      
-  def get_search_results(self, query: str, num: int=100):
-    """
-    Method which attempts to retrieve the search results for a query.
+        :param query: Query string based on which we want to retrieve the search results.
+        :param num: Number of results we want to retrieve.
     
-    :param query: Query string based on which we want to retrieve the search results.
-    :param num: Number of results we want to retrieve.
-    
-    """
-    
-    # Load existing articles from the Data Table, and display them in the articles_panel
-    if self.get_heartbeat():
-      try:
-        self.result_panel.items = anvil.server.call('get_pages', query=query, num=num)
-      except Exception as e:
-        print(f"Exception while trying to retrieve results for query '{query}': {e}")
-        self.show_error_notification(message="Couldn't retrieve results from the server!")
-        
-  def get_heartbeat(self) -> bool:
-    """Method which checks if the server is online."""
-    try:
-      return anvil.server.call('heartbeat')
-    except Exception as e:
-      print(f"Exception while trying to call heartbeat: {e}")
-      self.show_error_notification(message="Couldn't connect to server!")
-    
-    return False
-  
-  def show_error_notification(self, message: str):
-    """Method which shows an error notification."""
-    Notification(message,
-             title="Server error",
-             style="danger").show()
+        """
+
+        # Load existing articles from the Data Table, and display them in the articles_panel
+        if get_heartbeat():
+            try:
+                result = anvil.server.call('get_pages', query=query, num=num)
+            except Exception as e:
+                print(f"Exception while trying to retrieve results for query '{query}': {e}")
+                show_notification(message="Couldn't retrieve results from the server!")
+                return
+
+            if type(result) == str and result == "setting_up":
+                show_notification(message="Pages are currently being indexed. Try again later!",
+                                  style="warning")
+                return
+
+            self.result_panel.items = result
